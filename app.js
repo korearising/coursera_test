0d526cf75df7a4ce115b2222728693c328c0888f
kioto <kioto@coursera-smt.com>
2019-05-16 19:19:10 +0300

    changing app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function keyUp(event) {
 }
 //function studentName() {
 //  student.name = document.getElementById('name').value;
-//
+//}
 
 function calculateNumericOutput() {
   student.name = document.getElementById('name').value;
@@ -22,9 +22,11 @@ function calculateNumericOutput() {
   for (var i = 0; i < student.name.length; i++) {
     totalNameValue += student.name.charCodeAt(i);
   }
-}
-function Output(){
-  // Insert result into page
   var output = "Total Numeric value of person's name is " + totalNameValue;
   document.getElementById('output').innerText = output;
 }
+//function Output(){
+  // Insert result into page
+//  var output = "Total Numeric value of person's name is " + totalNameValue;
+//  document.getElementById('output').innerText = output;
+//}
